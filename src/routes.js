787c08a1df6d8b925a0ce2ff0b457873f253835b
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-02-26 10:10:48 -0300

    Aula 03 - Config Rotas (erro Switch agora é Routes)

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import Repository from "./pages/Repository";
+
+function Rotas() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/repository" element={<Repository />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Rotas;
